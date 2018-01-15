b61c2e9ef764e25a18269224bc7b4fee9b5390a3
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-01-15 17:33:35 -0600

    adds tests && passes for user model email and password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be invalid without an email" do
+    users(:one).email = nil
+    assert_nil users(:one).email
+    assert_equal false, users(:one).valid?
+   end
+
+   test "should be invalid without a encrypted password" do
+     users(:one).encrypted_password = nil
+     assert_nil users(:one).encrypted_password
+     assert_equal false, users(:one).valid?
+    end
 end
