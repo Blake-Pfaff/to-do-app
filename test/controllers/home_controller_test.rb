676dc53f27b73eed76b6b8568d9ae92da5d63d9b
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-01-23 15:52:18 -0600

    adds ability for users to sign in & redirects to user show path, adds tests for user and home ctrls and models

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -9,7 +9,9 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect an authenticated user to their show page" do
+    #:david_hasselhoff is from fixtures
     david = users(:david_hasselhoff)
+    #sign_in comes from include Devise::Test::IntegrationHelpers
     sign_in david
     get root_path
     assert_redirected_to user_path(david.id)
