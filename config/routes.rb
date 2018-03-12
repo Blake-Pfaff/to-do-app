b862aea7bb15f3a024ac8d3ffd55274abb3c169f
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-03-12 17:33:08 -0500

    adds landing page & re-route w/ devise to user#show after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  get '/users' => 'users#show', as: :users_root # creates user_root_path
+
+  namespace :users do
+    root 'users#show' # creates user_root_path
+  end
+
   resources :users
 
   root :to => 'home#index'
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,7 +11,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     # Visit the root path
     get root_path
     # Assert that the <div> with the ID "register-actions" exists on the page
-    assert_select "div#register-actions"
+    assert_select "#register-actions"
   end
 
   test "an authenticated user is not shown login / sign up links" do
@@ -22,17 +22,17 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     # Visit the root path
     get root_path
     # Verify that the <div> with the ID "register-actions" is not available
-    assert_select "div#register-actions", false
+    assert_select "#register-actions", false
     end
 
 
-  #test "should redirect an authenticated user to their show page" do
+  test "should redirect an authenticated user to their show page" do
     #:david_hasselhoff is from fixtures
-    #david = users(:david_hasselhoff)
+    david = users(:david_hasselhoff)
+    get root_path
     #sign_in comes from include Devise::Test::IntegrationHelpers
-    #sign_in david
-    #get root_path
-    #assert_redirected_to user_path(david.id)
-  #end
+    sign_in david
+    assert_redirected_to user_path(david.id)
+  end
 
 end
