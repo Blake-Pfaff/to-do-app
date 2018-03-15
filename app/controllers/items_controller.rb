cf8040a40fc8b279fc751102c5294f2d60f3d769
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-03-15 13:53:37 -0500

    adds flash logic and message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,23 @@
 class ItemsController < ApplicationController
-      before_action :set_user
+  before_action :set_user
 
-      def create
-        @item = @user.items.new(item_params)
-        @item.save
-        redirect_to root_path
-      end
+
+  def create
+    @item = @user.items.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Item was successfully created."
+    else
+      flash[:notice] = "item was not saved, please make to do item over 8 characters and under 101  "
+    end
+    redirect_back fallback_location: root_path
+  end
 
       private
 
-      def set_user
-        @user = User.find(params[:user_id])
-      end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
       def item_params
         params.require(:item).permit(:name)
