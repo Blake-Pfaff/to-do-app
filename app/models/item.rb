b009016fc5322f5a85be24e26d5f59a17bbb0582
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-03-27 16:07:27 -0500

    adds 7 day time limit to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   validates_length_of :name, :minimum => 8, :maximum => 100, :allow_blank => false
   belongs_to :user
+
+  def since
+    distance_of_time_in_words(self.created_at, 7.days.from_now)
+  end
+
 end
