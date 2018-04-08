77f274895a837b9704942ba399b57085b6a32c5e
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-04-08 16:37:12 -0500

    adds info to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'protected-bayou-26271.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -88,6 +96,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'protected-bayou-26271.herokuapp.com' }
 end
