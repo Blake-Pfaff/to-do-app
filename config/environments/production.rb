a7a66691306a587e55fc49071f0c9756c937516e
Blake Pfaff <blake.a.pfaff2@gmail.com>
2018-04-08 16:26:34 -0500

    tweeking mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'protected-bayou-26271.herokuapp.com' }
 end
